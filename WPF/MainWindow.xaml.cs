875d5ac937658f74b39734653898484be45a6c66
Gladwin Johnson <gljohns@microsoft.com>
2023-01-18 18:59:05 -0800

    updated app

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -23,40 +23,74 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly IEnumerable<string> s_scopes = new[] { "user.read" };
+
         public MainWindow()
         {
             InitializeComponent();
-
-            GetAccount("this-is-a-fake-client-id");
         }
 
         async void GetAccount(string clientId)
         {
-            IPublicClientApplication client = PublicClientApplicationBuilder
+            IPublicClientApplication pca = PublicClientApplicationBuilder
               .Create(clientId)
-              .WithAuthority("https://login.microsoftonline.com/consumers")
+              .WithAuthority("https://login.microsoftonline.com/common")
               .WithBrokerPreview(true)   // this method exists in Microsoft.Identity.Client.Broker package
               .Build();
 
+            IEnumerable<IAccount> accounts = await pca.GetAccountsAsync().ConfigureAwait(true);
+            var acc = accounts.SingleOrDefault();
+
             IntPtr windowHandle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
 
+            AuthenticationResult result = null;
             try
             {
-                var result = await client.AcquireTokenInteractive(new string[] { "openid",
-                "email",
-                "profile",
-                "offline_access", })
-                     .WithParentActivityOrWindow(windowHandle)
-                     .WithPrompt(Prompt.NoPrompt)
-                     .WithAccount(PublicClientApplication.OperatingSystemAccount)
-                     .ExecuteAsync();
-
-                MessageBox.Show(Convert.ToString(result), "", MessageBoxButton.OK, MessageBoxImage.Error);
+                result = await pca
+                    .AcquireTokenSilent(s_scopes, acc)
+                    .ExecuteAsync()
+                    .ConfigureAwait(false);
             }
-            catch(Exception ex)
+            catch (MsalUiRequiredException)
             {
-                MessageBox.Show(ex.Message+ex.StackTrace, "", MessageBoxButton.OK, MessageBoxImage.Error);
+                try
+                {
+                    var task = await Dispatcher.InvokeAsync(() =>
+                        pca.AcquireTokenInteractive(s_scopes)
+                        .WithParentActivityOrWindow(windowHandle)
+                        .WithAccount(acc)
+                        .ExecuteAsync());
+
+                    result = await task.ConfigureAwait(false);
+
+                }
+                catch (Exception ex3)
+                {
+                    DisplayMessage(ex3.Message.ToString());
+                    return;
+                }
             }
+            catch (Exception ex2)
+            {
+                DisplayMessage(ex2.Message.ToString());
+                return;
+            }
+
+            DisplayMessage($"Success! We have a token for {result.Account.Username} valid until {result.ExpiresOn}");
+        }
+
+        private void AtsAti_Click(object sender, RoutedEventArgs e)
+        {
+            GetAccount("4b0db8c2-9f26-4417-8bde-3f0e3656f8e0");
+        }
+
+        private void DisplayMessage(string message)
+        {
+            Dispatcher.Invoke(
+                   () =>
+                   {
+                       Log.Text = message;
+                   });
         }
     }
 }
